165bf5268a8398777183067a1d9690384ae11b06
Sardor <sardieyeee08@gmail.com>
2024-09-22 15:18:42 +0500

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function openModal() {
+    document.querySelector(".modal").classList.add("open-modal");
+}
+function closeModal() {
+    document.querySelector(".modal").classList.remove("open-modal");
+}
